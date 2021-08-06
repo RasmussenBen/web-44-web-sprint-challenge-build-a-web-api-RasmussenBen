f3a67ff7b4ae0ff04b4a44999e7f3b8f1b1d4544
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-06 12:13:59 -0500

    projects middleware wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,42 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model')
+
+function verifyProject (req, res, next) {
+    const { name, des } = req.body
+    if (!name || !name.trim() || !des || !des.trim()) {
+        res.status(400).json({ message: "Both Name and Description fields are required"})
+    }
+    req.name = name.trim()
+    req.des = des.trim()
+    next()
+}
+
+function verifyProjectComplete (req, res, next) {
+    const { name, des, complete } = req.body
+    if (!name || !name.trim() || !des || !des.trim() || !!complete) {
+        res.status(400).json({ message: "Both Name and Description fields are required"})
+    }
+    req.name = name.trim()
+    req.des = des.trim()
+    req.complete = complete
+    next()
+}
+
+async function verifyProjectId (req, res, next) {
+    try {
+        const specificProject = await Projects.get(req.params.id)
+        if (!specificProject) {
+            res.status(404).json({ message: "Specific project not found"})
+        }
+        req.specificProject = specificProject
+        next()
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+module.exports = {
+    verifyProject,
+    verifyProjectComplete,
+    verifyProjectId
+}
