9708e2f6121204ed4b91ee807063f0248be87861
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-06 12:52:43 -0500

    projects wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,10 +1,53 @@
 const express = require('express')
-
 const Projects = require('./projects-model')
-
+const { verifyProject, verifyProjectId } = require('./projects-middleware')
 const router = express.Router()
 
-function checkIdExists(req, res, next) {
-    const { id } = req.params
-    Projects.findById
-}
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then( projects => {
+            res.status(200).json(projects)
+        })
+        .catch(next)
+})
+
+router.get('/:id', verifyProjectId, (req, res) => {
+    res.json(req.project)
+})
+
+router.post('/', verifyProject, (req, res, next) => {
+    Projects.insert(req.body)
+        .then(newProject => {
+            res.status(201).json(newProject)
+        })
+        .catch(next)
+})
+
+router.put('/:id', verifyProjectId, verifyProject, (req, res, next) => {
+    Projects.update(req.params.id, req.body)
+        .then(() => {
+            return Projects.get(req.params.id)
+        })
+        .then(project => {
+            res.json(project)
+        })
+        .catch(next)
+})
+
+router.delete('/:id', verifyProjectId, async (req, res, next) => {
+    try {
+        await Projects.remove(req.params.id)
+        res.json(req.project)
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = router
